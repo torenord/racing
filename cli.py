f60426b63d298a07e5adf162c58788635adde442
Tore Norderud <tore.norderud@gmail.com>
2016-09-14 15:59:29 +0200

    Help and error handling

--- cli.py
+++ cli.py
@@ -14,21 +14,35 @@ while True:
     if inp == "q":
         break
 
+    if inp == "h":
+        print("q -- Quit")
+        print("h -- Help")
+        print("p -- Print current position")
+        print("l -- Load position from file")
+        print("s -- Save position from file")
+        print("h -- Print legal next positions")
+
     # Print current position
     elif inp == "p":
         print(position)
 
     # Load position from file
     elif inp.split(" ")[0] == "l":
-        filename = inp.split(" ")[1]
-        position = Position().parse(open(filename).read())
-        print(position)
+        try:
+            filename = inp.split(" ")[1]
+            position = Position().parse(open(filename).read())
+            print(position)
+        except:
+            pass
 
     # Save position from file
     elif inp.split(" ")[0] == "s":
-        filename = inp.split(" ")[1]
-        with open(filename, "w") as f:
-            f.write(repr(position))
+        try:
+            filename = inp.split(" ")[1]
+            with open(filename, "w") as f:
+                f.write(repr(position))
+        except:
+            pass
 
     # Print legal next positions
     elif inp == "n":
