eb07e7ba2934071e63d64d5bba5c149172aa1b8e
Tore Norderud <tore.norderud@gmail.com>
2016-09-14 18:03:01 +0200

    Allow for commands from command line

--- cli.py
+++ cli.py
@@ -1,19 +1,16 @@
-import readline
 from position import Position
+import readline
+import sys
 
 # Initial game position
 position = Position()
 
-while True:
-    try:
-        inp = input("> ")
-    except (EOFError, KeyboardInterrupt):
-        print()
-        break
+def handle(inp):
+    global position
 
     # Quit
     if inp == "q":
-        break
+        exit()
 
     # Help
     if inp == "h":
@@ -35,7 +32,7 @@ while True:
             filename = inp.split(" ")[1]
             position = Position().parse(open(filename).read())
             print(position)
-        except IndexError:
+        except (IndexError, ValueError):
             pass
 
     # Save position from file
@@ -44,10 +41,21 @@ while True:
             filename = inp.split(" ")[1]
             with open(filename, "w") as f:
                 f.write(repr(position))
-        except IndexError:
+        except (IndexError, ValueError):
             pass
 
     # Print legal next positions
     elif inp == "n":
         for p in position.legalmoves():
             print(p)
+if len(sys.argv) == 2:
+    handle(sys.argv[1])
+else:
+    while True:
+        try:
+            inp = input("> ")
+        except (EOFError, KeyboardInterrupt):
+            print()
+            break
+
+        handle(inp)
